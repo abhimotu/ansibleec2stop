915f3c7491dbbddbbf26ada0e84abc04c310034f
abhishek_kubetest <abhishek@example.com>
2021-04-26 18:28:39 +0530

    amended file7

--- deploystopec2.groovy
+++ deploystopec2.groovy
@@ -5,15 +5,19 @@ pipeline {
    stages {
       stage('Get Instance ID from AWS Private IPs') {
         steps {
+          sh """#!/bin/bash
           echo 'Retreiving...'
-          sh getInsID.sh
+          getInsID.sh
+        """
         }
    }
    stage('Power Down EC2') {
      steps {
+        sh """#!/bin/bash
         echo 'Powering Down EC2 $output.txt ...'
         ansible-vault decrypt secret-vars.yml --vault-password-file=pass.txt
         ansible-playbook stopec2ansible -e secret-vars.yml
+       """ 
     }
    }
   }
